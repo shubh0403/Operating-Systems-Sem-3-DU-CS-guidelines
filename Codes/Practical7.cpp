afca59b0b5fd515d05c835de0e18bdc0afeda46b
Shubh Sharma <sharma.shubh0401@gmail.com>
2022-11-29 18:36:06 +0530

    Practical 7 added

---
+++ Codes/Practical7.cpp
@@ -0,0 +1,131 @@
+// 7. Write a program to implement Round Robin scheduling algorithm.
+
+#include <iostream>
+using namespace std;
+
+void findWaitingTime(int processes[], int n,
+                     int bt[], int wt[], int quantum)
+{
+    int rem_bt[n];
+    for (int i = 0; i < n; i++)
+        rem_bt[i] = bt[i];
+
+    int t = 0; // Current time
+
+    while (1)
+    {
+        bool done = true;
+
+        for (int i = 0; i < n; i++)
+        {
+            // If burst time of a process is greater than 0
+            // then only need to process further
+            if (rem_bt[i] > 0)
+            {
+                done = false; // There is a pending process
+
+                if (rem_bt[i] > quantum)
+                {
+                    // Increase the value of t i.e. shows
+                    // how much time a process has been processed
+                    t += quantum;
+
+                    // Decrease the burst_time of current process
+                    // by quantum
+                    rem_bt[i] -= quantum;
+                }
+
+                // If burst time is smaller than or equal to
+                // quantum. Last cycle for this process
+                else
+                {
+                    // Increase the value of t i.e. shows
+                    // how much time a process has been processed
+                    t = t + rem_bt[i];
+
+                    // Waiting time is current time minus time
+                    // used by this process
+                    wt[i] = t - bt[i];
+
+                    // As the process gets fully executed
+                    // make its remaining burst time = 0
+                    rem_bt[i] = 0;
+                }
+            }
+        }
+
+        // If all processes are done
+        if (done == true)
+            break;
+    }
+}
+
+// Function to calculate turn around time
+void findTurnAroundTime(int processes[], int n,
+                        int bt[], int wt[], int tat[])
+{
+    // calculating turnaround time by adding
+    // bt[i] + wt[i]
+    for (int i = 0; i < n; i++)
+        tat[i] = bt[i] + wt[i];
+}
+
+// Function to calculate average time
+void findavgTime(int processes[], int n, int bt[],
+                 int quantum)
+{
+    int wt[n], tat[n], total_wt = 0, total_tat = 0;
+
+    // Function to find waiting time of all processes
+    findWaitingTime(processes, n, bt, wt, quantum);
+
+    // Function to find turn around time for all processes
+    findTurnAroundTime(processes, n, bt, wt, tat);
+
+    // Display processes along with all details
+    cout << " PN "
+         << " \tBT "
+         << " \tWT "
+         << " \tTAT\n";
+
+    // Calculate total waiting time and total turn
+    // around time
+    for (int i = 0; i < n; i++)
+    {
+        total_wt = total_wt + wt[i];
+        total_tat = total_tat + tat[i];
+        cout << " " << i + 1 << "\t" << bt[i] << "\t "
+             << wt[i] << "\t " << tat[i] << endl;
+    }
+
+    cout << "Average waiting time = "
+         << (float)total_wt / (float)n;
+    cout << "\nAverage turn around time = "
+         << (float)total_tat / (float)n;
+}
+
+// Driver code
+int main()
+{
+    int n;
+    cout << "Enter number of processes : ";
+    cin >> n;
+    int processes[n];
+    for (int i = 0; i < n; i++)
+    {
+        processes[i] = i + 1;
+    }
+
+    int burst_time[n];
+    cout << "Enter burst time of processes :- " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << i + 1 << " : ";
+        cin >> burst_time[i];
+    }
+
+    // Time quantum
+    int quantum = 2;
+    findavgTime(processes, n, burst_time, quantum);
+    return 0;
+}
